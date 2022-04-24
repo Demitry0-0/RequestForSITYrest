1080b8fd53c2d6da479b511a9c72c832e3c2def3
Demitry0-0 <58517416+Demitry0-0@users.noreply.github.com>
2022-04-25 02:39:44 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,5 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONValue;
 import org.json.simple.JSONObject;
 
 
@@ -27,7 +16,6 @@ public class Main {
 
 
     }
-
 
 
 }
--- Requests.java
+++ Requests.java
@@ -2,18 +2,12 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Requests {
     private final int CONNECTION_TIMEOUT = 5000;
